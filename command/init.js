e57055bab35371fa684478dfa45c5205c8354ebe
钱丹 <510043785@qq.com>
2016-12-02 18:53:27 +0800

    加了个获取git账户

--- command/init.js
+++ command/init.js
@@ -6,39 +6,70 @@ const multiline = prompt.multiline
 const config = require('../templates')
 const confirm = prompt.confirm
 const chalk = require('chalk')
-
+const inquirer = require('inquirer')
 module.exports = () => {
     co(function*() {
-        let isUseAdmin = yield prompt(chalk.green('\0 ?') + chalk.bold('\0 use admin-lte?') + chalk.grey(' (Y/n) \0'))
-        if (isUseAdmin == 'y' || isUseAdmin == 'Y') {
-            console.log(chalk.bold('\0 use admin-lte') + chalk.cyan(' Yes \0'))
-        } else {
-            console.log(chalk.cyan('\0 N'))
-        }
-        let tplName = yield prompt('Template name: ')
-        let projectName = yield prompt('Project name: ')
-        let gitUrl
-        let branch
-
-        if (!config.tpl[tplName]) {
-            console.log(chalk.red('\n × Template does not exit!'))
-            process.exit()
-        }
-        gitUrl = config.tpl[tplName].url
-        branch = config.tpl[tplName].branch
-
-        let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}`
-
-        console.log(chalk.white('\n Start generating...'))
-
-        exec(cmdStr, (error, stdout, stderr) => {
+        var cmd = 'git config --get user.name'
+        exec(cmd, (error, stdout, stderr) => {
             if (error) {
                 console.log(error)
                 process.exit()
             }
-            console.log(chalk.green('\n √ Generation completed!'))
-            console.log(`\n cd ${projectName} && npm install \n`)
-            process.exit()
+            console.log(stdout)
+        process.exit()
+    })
+        let questions = [{
+            type: 'list',
+            name: 'frame',
+            message: 'select the frame',
+            choices: [
+                {
+                    name:'vue-basic',
+                    value:1,
+                    sort:1
+                },
+                {
+                    name:'vue-wap-basic',
+                    value:2,
+                    sort:3
+                },
+                {
+                    name:'vue-mutipage-basic',
+                    value:3,
+                    sort:3
+                }
+            ]
+        },{
+            type:'input',
+            name:'projectName',
+            message:'Please enter your project name',
+            default:'my_project'
+        }];
+        inquirer.prompt(questions).then(function(answers) {
+            // console.log(JSON.stringify(answers, null, ''));
+            //
+            // var name=exec('git config --get user.name')
+            // var email=exec('git config --get user.email')
+            // console.log(name);
+            // name = name && name.toString().trim()
+            // email = email && (' <' + email.toString().trim() + '>')
+
+
+
+            let projectName = answers.projectName;
+            let gitUrl = 'git@git.kpi.pub:fe-basic/web-single-page-basic.git';
+            let cmdStr = `mkdir ${projectName} && cd ${projectName} && git clone ${gitUrl} `
+            console.log(chalk.white('\n Start generating...'))
+            exec(cmdStr, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(error)
+                    process.exit()
+                }
+                console.log(chalk.green('\n √ Generation completed!'))
+                console.log(`\n cd ${projectName} && npm install \n`)
+                process.exit()
+            })
         })
+
     })
 }
